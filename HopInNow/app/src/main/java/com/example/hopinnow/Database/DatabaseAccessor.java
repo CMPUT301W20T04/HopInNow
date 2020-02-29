102ec24dc7e203eca7c1c1dc2bd2b4b840bfdeda
Qianxi Li <qianxi@ualberta.ca>
2020-02-29 16:48:51 -0700

    Changed directory

--- HopInNow/app/src/main/java/com/example/hopinnow/Database/DatabaseAccessor.java
+++ HopInNow/app/src/main/java/com/example/hopinnow/Database/DatabaseAccessor.java
@@ -14,6 +14,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
+
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class DatabaseAccessor {
@@ -126,4 +127,6 @@ public class DatabaseAccessor {
     public FirebaseUser getUserProfile() {
         return firebaseAuth.getCurrentUser();
     }
+
+
 }
