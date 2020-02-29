de79c9b5926dfc41dc6468df8f67c8d77bf4d344
Shway Wang <shuwei4@ualberta.ca>
2020-02-28 23:40:22 -0700

    register prototype

--- HopInNow/app/src/main/java/com/example/hopinnow/DatabaseAccessor.java
+++ HopInNow/app/src/main/java/com/example/hopinnow/DatabaseAccessor.java
@@ -6,36 +6,41 @@ import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class DatabaseAccessor {
-    // Access a Cloud Firestore instance from your Activity
-    private FirebaseFirestore db;
+import java.util.HashMap;
 
-    public DatabaseAccessor () {
-        this.db = FirebaseFirestore.getInstance();
+class DatabaseAccessor {
+    public static final String TAG = "DatabaseAccessor";
+    // Access a Cloud Firestore instance from your Activity
+    private FirebaseFirestore database;
+
+    DatabaseAccessor() {
+        this.database = FirebaseFirestore.getInstance();
     }
 
-    public void signupRider (Rider rider) {
+    boolean signupRider(Rider rider) {
+        boolean flag = false;
         // get a top-level reference to the collection
-        final CollectionReference collectionReference = this.db.collection("Users");
-        collectionReference
+        this.database
+                .collection("Riders")
                 .document(rider.getName())
                 .set(rider)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
-                    public void onSuccess(Void aVoid) {
+                    public void onSuccess(Void v) {
                         // happens when data added successfully
-                        //Log.d(TAG, "Data addition successful!");
+                        Log.d(TAG, "Data addition successful!");
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
                         // happens when data added failed
-                        //Log.d(TAG, "Data addition failed" + e.toString());
+                        Log.d(TAG, "Data addition failed" + e.toString());
                     }
                 });
+        return flag;
     }
 }
--- HopInNow/app/src/main/java/com/example/hopinnow/LoginSignupActivity.java
+++ HopInNow/app/src/main/java/com/example/hopinnow/LoginSignupActivity.java
@@ -2,16 +2,41 @@ package com.example.hopinnow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginSignupActivity extends AppCompatActivity {
     public static final String TAG = "LoginSignupActivity";
+    EditText nameEditText;
+    EditText passwordEditText;
+    EditText phoneNumberEditText;
+    EditText emailEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_signup_view);
 
+        nameEditText = findViewById(R.id.name);
+        passwordEditText = findViewById(R.id.password);
+        phoneNumberEditText = findViewById(R.id.phoneNumber);
+        emailEditText = findViewById(R.id.email);
+    }
+
+    public void signup(View view) {
+        final DatabaseAccessor databaseAccessor = new DatabaseAccessor();
+
+        String name = nameEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String phoneNumber = phoneNumberEditText.getText().toString();
+        String email = emailEditText.getText().toString();
+
+        Rider rider = new Rider(name, password, phoneNumber, email);
+        rider.setUserDeposit(100);
+        databaseAccessor.signupRider(rider);
     }
 }
