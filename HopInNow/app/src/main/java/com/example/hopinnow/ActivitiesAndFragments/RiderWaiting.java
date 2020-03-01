6aa51eaac391cb3a1aa590b044241d9119f7d919
Recful <59626497+Recful@users.noreply.github.com>
2020-02-29 17:29:42 -0700

    rider_waiting

---
+++ HopInNow/app/src/main/java/com/example/hopinnow/ActivitiesAndFragments/RiderWaiting.java
@@ -0,0 +1,20 @@
+package com.example.hopinnow.ActivitiesAndFragments;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.hopinnow.R;
+
+public class RiderWaiting extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.waiting);
+    }
+
+    public void waiting(View view){
+
+    }
+}
