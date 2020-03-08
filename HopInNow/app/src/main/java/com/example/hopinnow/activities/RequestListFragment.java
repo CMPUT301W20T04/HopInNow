345c088b9b9cb4eac3c4681efda670b202335a1a
Hongru Qi <hqi@ualberta.ca>
2020-03-08 12:32:06 -0600

    get new

--- HopInNow/app/src/main/java/com/example/hopinnow/activities/RequestListFragment.java
+++ HopInNow/app/src/main/java/com/example/hopinnow/activities/RequestListFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -11,14 +13,20 @@ import androidx.fragment.app.FragmentActivity;
 
 import com.example.hopinnow.R;
 import com.example.hopinnow.entities.Request;
+import com.example.hopinnow.entities.RequestListAdapter;
 
 import java.util.ArrayList;
 
-public class RequestListFrament extends Fragment {
+public class RequestListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_driver_requests, container, false);
         ArrayList<Request> requestList = new ArrayList<Request>();
-        
+        final FragmentActivity fragmentActivity = getActivity();
+
+        RequestListAdapter adapter = new RequestListAdapter(requestList, fragmentActivity);
+        ListView lView = (ListView)fragmentActivity.findViewById(R.id.requestList);
+        lView.setAdapter(adapter);
+
 
         return view;
     }
