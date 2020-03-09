aed83c5e3d850260314323ccdf3b2a6cc5d86534
viola <violabaity@gmail.com>
2020-03-09 16:25:44 -0600

    _comments Viola

--- HopInNow/app/src/main/java/com/example/hopinnow/activities/RiderMapActivity.java
+++ HopInNow/app/src/main/java/com/example/hopinnow/activities/RiderMapActivity.java
@@ -215,13 +215,15 @@ public class RiderMapActivity extends FragmentActivity implements OnMapReadyCall
     @Override
     protected void onStart(){
         super.onStart();
-        searchInPlace = false;
         Bundle b = getIntent().getExtras();
 
         if(b!=null) {
-            if (Boolean.parseBoolean(Objects.requireNonNull(b.get("Current_Request_To_Null")).toString())){
+            Log.e("getting bundle","bundle not null");
+            if (Boolean.parseBoolean(b.get("Current_Request_To_Null").toString())){
+                Log.e("getting bundle","cancel is done");
                 cancelRequestLocal();
             }
+
         }
 
         if (curRequest!=null) {
@@ -229,6 +231,17 @@ public class RiderMapActivity extends FragmentActivity implements OnMapReadyCall
             curRequest = retrieveCurrentRequestLocal();
             searchFragment.setVisibility(View.GONE);
             searchInPlace = false;
+        } else {
+            searchInPlace = true;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (curRequest != null) {
+            //mMap.clear();
+            curRequest = retrieveCurrentRequestLocal();
         }
     }
 
