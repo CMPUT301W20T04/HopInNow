b586488b99df3b826eb1d33bf19360b86a4a328e
viola <violabaity@gmail.com>
2020-03-11 11:39:40 -0600

    goodbye rider activity robotium

--- HopInNow/app/src/androidTest/java/com/example/hopinnow/RegisterActivityTest.java
+++ HopInNow/app/src/androidTest/java/com/example/hopinnow/RegisterActivityTest.java
@@ -1,4 +1,6 @@
 package com.example.hopinnow;
 
 public class RegisterActivityTest {
+    //TODO all edittext
+    //TODO driver car edittext
 }
--- HopInNow/app/src/androidTest/java/com/example/hopinnow/RiderMenuActivityTest.java
+++ HopInNow/app/src/androidTest/java/com/example/hopinnow/RiderMenuActivityTest.java
@@ -1,4 +1,6 @@
 package com.example.hopinnow;
 
 public class RiderMenuActivityTest {
+    //TODO MY TRIPS
+    //TODO MY PROFILE: update, logout
 }
