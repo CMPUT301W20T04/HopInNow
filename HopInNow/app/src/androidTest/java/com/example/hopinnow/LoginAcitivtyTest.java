44434bdf508cce227583f5c8d31edfa10d99954d
viola <violabaity@gmail.com>
2020-03-11 14:45:12 -0600

    intent testing so much fun

--- HopInNow/app/src/androidTest/java/com/example/hopinnow/LoginAcitivtyTest.java
+++
@@ -1,6 +0,0 @@
-package com.example.hopinnow;
-
-public class LoginAcitivtyTest {
-    //TODO WRONG ACCOUNT THEN RIGHT ACCOUNT
-    // NOTE: previous use must be logged out to login properly, else user that DNE can still log in
-}
