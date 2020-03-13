8b3d1919f098f272516b4183fe8094a872c24850
Hongru Qi <hqi@ualberta.ca>
2020-03-13 04:05:04 -0600

    add offline

--- HopInNow/app/src/main/java/com/example/hopinnow/activities/DriverMenuActivity.java
+++ HopInNow/app/src/main/java/com/example/hopinnow/activities/DriverMenuActivity.java
@@ -59,7 +59,10 @@ public class DriverMenuActivity extends AppCompatActivity implements DriverProfi
         offlineBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(getApplicationContext(), DriverMapActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
             }
         });
 
