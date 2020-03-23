9d3adb5e41423d04b9d81836a4004ed02b41f392
Qianxi Li <qianxi@ualberta.ca>
2020-03-22 23:54:28 -0600

    frontPage

---
+++ HopInNow/app/src/main/java/com/example/hopinnow/activities/firstPageActivity.java
@@ -0,0 +1,62 @@
+package com.example.hopinnow.activities;
+
+import android.annotation.SuppressLint;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.hopinnow.R;
+//https://stackoverflow.com/questions/17357226/add-the-loading-screen-in-starting-of-the-android-application
+
+public class firstPageActivity extends AppCompatActivity {
+    private ImageView titleImage;
+    private ImageView roadImage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_page);
+        titleImage = findViewById(R.id.titleImage);
+        roadImage = findViewById(R.id.roadImage);
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        Thread welcomeThread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(2000);  //Delay of 10 seconds
+                } catch (Exception e) {
+                } finally {
+                    Intent i = new Intent(firstPageActivity.this,
+                            PagerActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        welcomeThread.start();
+
+
+    }
+
+
+
+}
